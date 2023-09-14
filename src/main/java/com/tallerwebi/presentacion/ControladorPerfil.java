49e2be2fa7af95f9376ae2c2356f8494098831b5
MonteagudoRodrigo <71356504+MonteagudoRodrigo@users.noreply.github.com>
2023-09-14 05:24:47 -0300

    creados los archivos

---
+++ src/main/java/com/tallerwebi/presentacion/ControladorPerfil.java
@@ -0,0 +1,19 @@
+package com.tallerwebi.presentacion;
+
+import com.tallerwebi.dominio.Usuario;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ControladorPerfil {
+
+    @RequestMapping(path = "/nuevo-usuario", method = RequestMethod.GET)
+    public ModelAndView nuevoUsuario() {
+        ModelMap model = new ModelMap();
+        model.put("usuario", new Usuario());
+        return new ModelAndView("nuevo-usuario", model);
+    }
+}
